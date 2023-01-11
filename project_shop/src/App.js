342e9483bf2d7b6f79589dbe28e82a4adea57953
eunkyeong <eunkyeong91@hanmail.net>
2023-01-11 10:04:52 +0900

    exprot,import

--- project_shop/src/App.js
+++ project_shop/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 //bottstrap 설치, 활용 
 import {Nav, Navbar, NavDropdown, Container} from 'react-bootstrap';
+
 //html에서 image 가져다 쓸때 꼭 import로 불러온 다음 사용해야함
 //상대주소가 있는 이미지의 경우 그냥 src="절대경로 주소" 넣으면 됨
 //public 폴더 아마자를 쓸때에는 <img src={process.env.PUBLIC_URL + '이미지경로'} 이렇게 쓰면됨
 // import mainVisual from './img/main01.jpeg';
 
+import product from './data';
+/*export, import
+다른 파일에 있는 함수, 변수를 가져올때 사용한다. 
+한개의 함수를 내보낼때는 export default 변수명;
+-> import 변수명(내보낸 이름과 동일하지 않아도 됨) from '파일경로' js 파일의 경우, js을 생략해도된다.
+한개 이상의 함수를 내보낼때는 export {변수명, 변수명};
+->import {변수명, 변수명} from '파일경로'; 로 적어주면된다.
+*/
 function App() {
   return (
     <div className="App">
@@ -28,12 +37,24 @@ function App() {
       </Container>
     </Navbar>
 
-    <div className='visual'>
+    <div className='visual'></div>
+    <ul className='productList'>
 
-    </div>
+    </ul>
 
     </div>
   );
 }
+
+const products = (props)=>{
+  return <li>
+    <img src={process.env.PUBLIC_URL + '../public/img/bag'}></img>
+    <dl>
+      <dt>{props}</dt>
+      <dd>{props}</dd>
+      <dd>{props}</dd>
+    </dl>
+  </li>
+} 
 
 export default App;
