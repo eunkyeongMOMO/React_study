7ac3042e2449a1dafb21a6b92378300d414dc10b
eunkyeong <eunkyeong91@hanmail.net>
2023-01-13 13:07:49 +0900

    230113_오전

--- project_shop/src/Detail.js
+++ project_shop/src/Detail.js
@@ -1,4 +1,28 @@
 import { useParams } from "react-router-dom"
+import styled from "styled-components"
+
+/* 스타일 컴포넌트 라이브러리 사용하기 
+npm intall style-components 입력하여 라이브러리 설치
+사용할 js 파일에 import styled from "styled-components" 입력
+let 컴포넌트명 = styled.태그명 `스타일 속성:값 작성`(백틱)
+props속성을 이용하여 요소별로 바꿀수있음
+props로 지정할 속성: ${props => props.props명} 
+장점이자 단점. 해당 파일에서 밖에 못씀. 쓰려면 export, import해야함.
+-> css와 뭐가 다른지?
+css 파일을 특정 컴포넌트에서만 사용하고 싶을때 
+css 파일명 -> 컴포넌트명.module.css로 적으면 해당 컴포넌트에만 적용됨.
+*/
+let Button =styled.button`
+backgroud: ${ props => props.back};
+color:#fff;
+border-radius: 10px;
+padding: 10px 20px;
+`
+//기존 스타일 복사 + 추가
+let NewButton = styled.button(Button)`
+margin:20px auto;
+`
+
 
 const Detail = (props) => {
     let {id} = useParams()
@@ -8,6 +32,7 @@ const Detail = (props) => {
 
     return  <div className="container">
       <div className="row">
+        <Button back="orange"></Button>
         <div className="col-md-6">
         <img src={process.env.PUBLIC_URL + './img/bag'+(findProduct)+'.webp'} width="100%"></img>
         </div>
