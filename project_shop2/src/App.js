c43a322d0bea777c3e218b90ec7d3b7e42a57193
eunkyeong <eunkyeong91@hanmail.net>
2023-01-13 13:59:13 +0900

    Router

--- project_shop2/src/App.js
+++ project_shop2/src/App.js
@@ -1,8 +1,27 @@
+import {Route, Routes, link, useNavigate, Outlet} from 'react-router-dom';
+import {Navbar,Container,Nav} from 'react-bootstrap';
 import './App.css';
-
+import bagData from './data';
+import Main from './main';
 function App() {
+  let navigate=useNavigate();
+
   return (
     <div className="App">
+        <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Main></Main>
+      {/* <Routes>
+        <Route path='/' element={<Main bagData={bagData}/>}/>
+      </Routes> */}
     </div>
   );
 }
