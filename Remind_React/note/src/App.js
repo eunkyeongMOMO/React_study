4789dcb489dacaf0e850320e3c18bfa58396a9fb
eunkyeong <eunkyeong91@hanmail.net>
2023-03-21 21:16:50 +0900

    docs. 230321_ReactRemind_ Component

--- Remind_React/note/src/App.js
+++ Remind_React/note/src/App.js
@@ -12,27 +12,36 @@ function App() {
           <h1>My_Note</h1>
         </div>
       </header>
-    <ul>
+    <ul className='postList'>
       <li>
         <p>{posts[0]}</p>
         <span onClick={()=>{likesModify(likes+1)}}>❤️</span>
-        {likes}
+        <span>{likes}</span>
       </li>
       <li>
         <p>{posts[1]}</p>
         <span onClick={()=>{likesModify(likes+1)}}>❤️</span>
-        {likes}
+        <span>{likes}</span>
       </li>
       <li>
         <p>{posts[2]}</p>
         <span onClick={()=>{likesModify(likes+1)}}>❤️</span>
-        {likes}
+        <span>{likes}</span>
       </li>
     </ul>
-
 
     </div>
   );
 }
 
+const Modal = ()=>{
+  return(
+      <div className="modal">
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+
+  )
+}
 export default App;
