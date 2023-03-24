9a36ec4a97c09254fa9abba7c33da757f1e2abf2
eunkyeong <eunkyeong91@hanmail.net>
2023-03-24 22:46:23 +0900

    add.230324_Rmind_delete

--- Remind_React/note/src/App.js
+++ Remind_React/note/src/App.js
@@ -7,22 +7,36 @@ App.prototype ={
 }
 
 function App() {
-  let [posts, postsModify] = useState(['기본','연습','저장']);
-  let [likes, likesModify]=useState([0,0,0]);
+  let [posts, setPosts] = useState(['첫번째','두번째','세번째']);
+  let [likes, setLikes]= useState([0,0,0]);
   let [modal, setmodal] = useState(false);
   let [title, settitle] =useState(0);
+  let [inputValue, setInputValue] =useState('');
+
 
   let likePlus = (index) =>{
     let copyLikes = [...likes];
     copyLikes[index]= copyLikes[index]+1;
-    likesModify(copyLikes);
+    setLikes(copyLikes);
   }
   let subjectModify = () =>{
     let copy =[...posts];
     copy[0]='수정했습니다.';
-    postsModify(copy);
+    setPosts(copy);
   }
-  
+  let addPost = ()=>{
+    let copy = [...posts];
+    copy.unshift(inputValue);
+    setPosts(copy);
+  }
+  let deletePost = (i)=>{
+    let copy =[...posts];
+    copy.splice(i,1)
+    setPosts(copy);
+    let copyLikes =[...likes]
+    copyLikes.splice(i,1)
+    setLikes(copyLikes);
+  }
   return (
     <div className="App">
       <header>
@@ -37,13 +51,17 @@ function App() {
           return ( 
           <li key={index} onClick={()=>{setmodal(!modal)}}>
             <p onClick={()=>{settitle(index)}}>{value}</p>
-            <span onClick={()=>{likePlus(index)}}>❤️</span>
+            <span onClick={(e)=>{
+              e.stopPropagation();
+              likePlus(index)}}>❤️</span>
             <span>{likes[index]}</span>
+            <button onClick={(e)=>{e.stopPropagation(); deletePost(index)}}>글삭제</button>
           </li>)
         })
    }
       </ul>
-   <button onClick={subjectModify}>글제목 변경</button>
+      <input onChange={(e)=>{setInputValue(e.target.value)}}></input>
+      <button onClick={addPost}>글추가</button>
       {
       modal == true ? <Modal title={title} subjectModify={subjectModify} posts={posts}></Modal> : null 
       }
